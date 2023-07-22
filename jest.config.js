e3c18f181a5a2361331c021c9dfd29d7087b910c
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-07-21 21:42:27 -0700

    build(version): svelte 4 support
    BREAKING CHANGE: Now the library support svelte 4

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
-module.exports = {
+export default {
+  testEnvironment: "jsdom",
   transform: {
-    "^.+\\.svelte$": "svelte-jester",
+    // TODO: change this value to just "svelte-jester" with the next release of "svelte-jester"
+    "^.+\\.svelte$": "./node_modules/svelte-jester/dist/transformer.mjs",
     "^.+\\.js$": "babel-jest",
   },
   moduleFileExtensions: ["js", "svelte"],
+  extensionsToTreatAsEsm: [".svelte", ".ts"],
   collectCoverage: true,
 };
